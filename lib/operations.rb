21a7a6293d975255cecb5d689eeb72f9fe7dfbea
Annie Truex <artruex@gmail.com>
2019-07-10 23:07:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,2 @@
-def unsafe?(speed)
-  if speed > 60
-    "true"
-  else speed < 40
-    "true"
-  elsif speed
-    "false"
-end
-
-
-
-def not_safe?(speed)
-	speed > 60 : "true" : "fasle"
-end
-	
 
 
